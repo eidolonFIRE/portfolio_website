e06a0b9b551541d0a2a3f40f7d2e71df7c0b44bb
eidolonFIRE <c4leb.johnson@gmail.com>
2016-08-30 21:08:22 -0700

    working

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,27 @@ $(document).ready(function(){
 	$(".artBtn").click(function()
 	{
 		$("#articlePanel").load("articles/"+$(this).attr("value")+"/"+$(this).attr("value")+".html");
+		// history.pushState({}, '', "#" + $(this).attr("value"));
+		location.hash = $(this).attr("value")
 	});
 
+
+	loadArticle = function(hash) {
+		hash = hash.substr(1)
+		$("#articlePanel").load("articles/"+hash+"/"+hash+".html");
+	};
+
+	loadArticle(location.hash);
+
+
+	// $(window).on("popstate", function(e) {
+	// 	if (e.originalEvent.state !== null) {
+	// 		loadArticle(location.hash);
+	// 	}
+	// });
+
+	$(window).on("popstate", function() {
+		loadArticle(location.hash);
+	});
 
 })
