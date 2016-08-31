b7b721981e80e605aad3d0f8e7e92ca19d50a0b8
eidolonFIRE <c4leb.johnson@gmail.com>
2016-08-30 20:03:11 -0700

    working

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 	$("#article1").click(function()
 	{
-		$("#articlePanel").load('articles/article1/article1.html');
+		$("#articlePanel").load("articles/article1/article1.html");
 	});
 
 })
